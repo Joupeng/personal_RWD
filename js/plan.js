0cb82b281bda546cb8fbd43fa4d0dccf4ba4f8f1
Joupeng <pengchih10@gmail.com>
2023-10-23 18:54:34 +0800

    about us change photo

---
+++ js/plan.js
@@ -0,0 +1,10 @@
+function doFirst() {
+    document.getElementById('myForm_create').onsubmit = calculate
+    //跟HTML畫面產生關聯，在建立事件聆聽功能()
+}
+function calculate(e) {
+    e.preventDefault()
+    let start = document.getElementById()
+}
+
+window.addEventListener('load', doFirst)
